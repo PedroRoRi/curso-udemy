9c696f5b19cb279a79658bb7b34bede2be2e5a79
PedroRoRi <ing.p.rodriguez4@gmail.com>
2024-02-09 23:03:16 -0500

    Commit primera parte

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+//import PrimerComponente from "./Components/PrimerComponente";
+import Props from "./Components/Props";
+
+function App() {
+  return (
+    <Fragment>
+      <Props></Props>
+      {/* <PrimerComponente></PrimerComponente> */}
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Props.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Props extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Props</h1>
+      </div>
+    );
+  }
+}
